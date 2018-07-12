6de6dc93132943b0c6b9f5f92babf62cb72d2b1e
Joel M <hj923@hotmail.com>
2018-07-12 16:48:03 +0000

    add styles (theme)

--- src/dist/YoutubeAutocomplete.js
+++ src/dist/YoutubeAutocomplete.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Core from '../components/Core'
-import MuiThemeProvider from '@material-ui/styles'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import blue from '@material-ui/core/colors/blue'
 
-const YoutubeAutocomplete = ({ theme }) => {
-  ;<MuiThemeProvider theme={theme}>
-    <Core />
-  </MuiThemeProvider>
+const YoutubeAutocomplete = ({
+  theme = createMuiTheme({
+    palette: {
+      primary: blue,
+    },
+  }),
+}) => {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <Core />
+    </MuiThemeProvider>
+  )
+}
+
+YoutubeAutocomplete.propTypes = {
+  theme: PropTypes.func.isRequired,
 }
 
 export default YoutubeAutocomplete
