ec6d60cf99d76b01d9787f8d426789223a85e7e2
Joel M <hj923@hotmail.com>
2018-07-18 17:50:44 +0000

    refactor scripts

---
+++ src/dev/wrapper/YoutubeAutocomplete.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Core from '../components/Core'
+
+/*
+  Kind of a wrapper. Provided for future extension to other functions. 
+*/
+
+const YoutubeAutocomplete = ({
+  useMui,
+  inputId,
+  menuId,
+  itemClassName,
+  theme,
+}) => {
+  return (
+    <Core
+      useMui={useMui}
+      inputId={inputId}
+      menuId={menuId}
+      itemClassName={itemClassName}
+      theme={theme}
+      apiKey=""
+    />
+  )
+}
+
+YoutubeAutocomplete.propTypes = {
+  theme: PropTypes.func.isRequired,
+}
+
+export default YoutubeAutocomplete
