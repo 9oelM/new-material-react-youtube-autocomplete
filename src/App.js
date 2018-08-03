3dbe5fa6519aebb386b5cf5cef34d061c883c82d
Joel M <hj923@hotmail.com>
2018-08-03 14:40:58 +0000

    add cards

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import YoutubeAutocomplete from './dist/Core'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 import { withStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
 import 'typeface-roboto'
+import RenderSearchResults from './RenderSearchResults'
+import './App.css'
+import './Width'
 
 const styles = {
   container: {
     display: 'flex',
     height: '100%',
     width: '100%',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
+    flexDirection: 'column',
   },
   paper: {
     maxWidth: 500,
@@ -22,10 +27,13 @@ const styles = {
   heading: {
     paddingBottom: '10px',
   },
+  results: {
+    marginTop: '15px',
+  },
   '@media (max-height: 1024px)': {
     container: {
-      justifyContent: 'center',
-      alignItems: 'start',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
     },
     paper: {
       maxWidth: 'initial',
@@ -37,32 +45,51 @@ const styles = {
 }
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      searchResults: [],
+      searchWord: '',
+    }
+  }
+
   render() {
     const { classes } = this.props
     return (
-      <div className={classes.container}>
-        <Paper className={`app ${classes.paper}`} square>
-          <Typography
-            variant="headline"
-            component="h1"
-            className={classes.heading}
-          >
-            new-material-react-youtube-autocomplete
-          </Typography>
-          <YoutubeAutocomplete
-            option={{
-              maxResults: 15,
-              type: ['video', 'playlist'],
-              key: 'AIzaSyB8R4Bqkx25_-c58L7v1QaLReVw1FWea28',
-            }}
-            onSuggestError={error => console.log(`error: ${error}`)}
-            onSearchError={error => console.log(`error: ${error}`)}
-            onSearchResults={result => console.log(result)}
-            onSearchTrigger={inputValue =>
-              console.log(`${inputValue} is being searched.`)
-            }
-          />
-        </Paper>
+      <div>
+        <Grid className={classes.container} container>
+          <Paper className={`app ${classes.paper}`} square>
+            <Typography
+              id="heading"
+              variant="headline"
+              component="h1"
+              className={classes.heading}
+            >
+              new-material-react-youtube-autocomplete
+            </Typography>
+            <YoutubeAutocomplete
+              option={{
+                maxResults: 15,
+                type: ['video', 'playlist'],
+                key: 'AIzaSyB8R4Bqkx25_-c58L7v1QaLReVw1FWea28',
+              }}
+              onSuggestError={error => console.log(`error: ${error}`)}
+              onSearchError={error => console.log(`error: ${error}`)}
+              onSearchResults={result =>
+                this.setState({ searchResults: result })
+              }
+              onSearchTrigger={inputValue =>
+                this.setState({ searchWord: inputValue })
+              }
+            />
+          </Paper>
+          <Paper id="results" className={classes.results} square>
+            <RenderSearchResults
+              searchWord={this.state.searchWord}
+              searchResults={this.state.searchResults}
+            />
+          </Paper>
+        </Grid>
       </div>
     )
   }
